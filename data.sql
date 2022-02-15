af3450c272b55b43fad68d8ff7bc5b1d3487866b
Meri <mgogi13@freeuni.edu.ge>
2022-02-15 18:11:10 +0400

    Add query multiple tables

--- data.sql
+++ data.sql
@@ -1,3 +1,4 @@
+-- insert Data into Animals table
 INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg)
 VALUES (1, 'Agumon', '2020-02-03', 0, true, 10.23);
 
@@ -27,3 +28,59 @@ VALUES (9, 'Boarmon', '2005-06-07', 7, true, 20.4);
 
 INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg)
 VALUES (10, 'Blossom', '1998-02-13', 3, true, 17);
+
+-- insert Data into Owners table
+INSERT INTO owners(full_name, age)
+VALUES ('Sam Smith', 34);
+
+INSERT INTO owners(full_name, age)
+VALUES ('Jennifer Orwell', 19);
+
+INSERT INTO owners(full_name, age)
+VALUES ('Bob', 45);
+
+INSERT INTO owners(full_name, age)
+VALUES ('Melody Pond', 77);
+
+INSERT INTO owners(full_name, age)
+VALUES ('Dean Winchester', 14);
+
+INSERT INTO owners(full_name, age)
+VALUES ('Jodie Whittaker', 38);
+
+-- insert Data into Species table
+INSERT INTO species(name)
+VALUES ('Pokemon');
+
+INSERT INTO species(name)
+VALUES ('Digimon');
+
+-- Modify inserted animals for species_id column
+UPDATE animals
+SET species_id = 2
+WHERE name LIKE '%mon';
+
+UPDATE animals
+SET species_id = 1
+WHERE name NOT LIKE '%mon';
+
+-- Modify inserted animals for owner_id column
+UPDATE animals
+SET owner_id = 1
+WHERE name = 'Agumon';
+
+UPDATE animals
+SET owner_id = 2
+WHERE name IN ('Pikachu', 'Gabumon');
+
+UPDATE animals
+SET owner_id = 3
+WHERE name IN ('Devimon', 'Plantmon');
+
+UPDATE animals
+SET owner_id = 4
+WHERE name IN ('Charmander', 'Squirtle', 'Blossom');
+
+UPDATE animals
+SET owner_id = 5
+WHERE name IN ('Angemon', 'Boarmon');
--- queries.sql
+++ queries.sql
@@ -15,6 +15,9 @@ SELECT * FROM animals WHERE name != 'Gabumon';
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.4;
 
 -- Transactions
+-- Inside a transaction update the animals table by setting the species column to 
+-- unspecified. Verify that change was made. Then roll back the change and verify that 
+-- species columns went back to the state before transaction.
 BEGIN;
 UPDATE animals
 SET species = 'unspecified';
@@ -22,12 +25,16 @@ SELECT species from animals;
 ROLLBACK;
 SELECT species from animals;
 
+-- Update the animals table by setting the species column to digimon 
+-- for all animals that have a name ending in mon.
 BEGIN;
 UPDATE animals 
 SET species = 'digimon'
 WHERE name LIKE '%mon';
 SELECT species from animals;
 
+-- Update the animals table by setting the species column to 
+-- pokemon for all animals that don't have species already set.
 BEGIN;
 UPDATE animals 
 SET species = 'pokemon'
@@ -35,21 +42,26 @@ WHERE name NOT LIKE '%mon';
 COMMIT;
 SELECT species from animals;
 
+-- Inside a transaction delete all records in the animals table, then roll back the transaction.
 BEGIN;
 DELETE FROM animals;
 ROLLBACK;
 SELECT * from animals;
 
+-- Delete all animals born after Jan 1st, 2022
 BEGIN;
 DELETE FROM animals
 WHERE date_of_birth > '2022-01-01';
+-- Create a savepoint for the transaction
 SAVEPOINT del1;
 
+-- Update all animals' weight to be their weight multiplied by -1
 BEGIN;
 UPDATE animals
 SET weight_kg = weight_kg * (-1);
 ROLLBACK TO SAVEPOINT del1;
 
+-- Update all animals' weights that are negative to be their weight multiplied by -1
 BEGIN;
 UPDATE animals
 SET weight_kg = weight_kg * (-1)
@@ -58,19 +70,67 @@ COMMIT;
 SELECT * from animals;
 
 -- Write queries to answer the questions
+-- How many animals are there?
 SELECT COUNT(*) FROM animals; //10
 
+-- How many animals have never tried to escape?
 SELECT COUNT(escape_attempts) FROM animals
 WHERE escape_attempts = 0; //2
 
+-- What is the average weight of animals?
 SELECT AVG(weight_kg) FROM animals; //15.55...
 
+-- Who escapes the most, neutered or not neutered animals?
 SELECT neutered, AVG(escape_attempts) FROM animals
 GROUP BY neutered;
 
+-- What is the minimum and maximum weight of each type of animal?
 SELECT species, MAX(weight_kg), MIN(weight_kg) FROM animals
 GROUP BY species;
 
+-- What is the average number of escape attempts per animal type of those born between 1990 and 2000?
 SELECT species, AVG(escape_attempts) FROM animals
 WHERE date_of_birth BETWEEN '1990-01-01' AND '2001-01-01'
 GROUP BY species;
+
+-- answer questions
+-- What animals belong to Melody Pond?
+SELECT name FROM animals
+JOIN owners
+ON animals.owner_id = owners.id
+WHERE full_name = 'Melody Pond';
+
+-- List of all animals that are pokemon (their type is Pokemon)
+SELECT animals.name FROM animals
+JOIN species
+ON animals.species_id = species.id
+WHERE species_id = 1
+
+-- List all owners and their animals, remember to include those that don't own any animal
+SELECT name, full_name FROM animals
+RIGHT JOIN owners
+ON animals.owner_id = owners.id
+
+-- How many animals are there per species?
+SELECT COUNT(*) FROM animals
+JOIN species
+ON animals.species_id = species.id 
+GROUP BY species;
+
+-- List all Digimon owned by Jennifer Orwell
+SELECT name FROM animals
+JOIN owners
+ON animals.owner_id = owners.id 
+WHERE full_name = 'Jennifer Orwell' AND species_id = 2;
+
+-- List all animals owned by Dean Winchester that haven't tried to escape
+SELECT name FROM animals
+JOIN owners
+ON animals.owner_id = owners.id 
+WHERE full_name = 'Dean Winchester' AND escape_attempts = 0;
+
+-- Who owns the most animals?
+SELECT COUNT(*) FROM animals
+JOIN owners
+ON animals.owner_id = owners.id 
+GROUP BY owners.full_name;
