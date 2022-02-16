f41b4a189bb8879e2dfeea2f5bc419746387cb58
Meri <mgogi13@freeuni.edu.ge>
2022-02-16 14:34:51 +0400

    add join table for visits

--- data.sql
+++ data.sql
@@ -84,3 +84,90 @@ WHERE name IN ('Charmander', 'Squirtle', 'Blossom');
 UPDATE animals
 SET owner_id = 5
 WHERE name IN ('Angemon', 'Boarmon');
+
+-- insert data into vets
+INSERT INTO vets(name, age, date_of_graduation)
+VALUES ('William Tatcher', 45, '2000-04-23');
+
+INSERT INTO vets(name, age, date_of_graduation)
+VALUES ('Maisy Smith', 26, '2019-01-17');
+
+INSERT INTO vets(name, age, date_of_graduation)
+VALUES ('Stephanie Mendez', 64, '1981-05-04');
+
+INSERT INTO vets(name, age, date_of_graduation)
+VALUES ('Jack Harkness', 38, '2008-06-08');
+
+-- insert data into specializations:
+INSERT INTO specializations(vets_id, species_id)
+VALUES (1, 1);
+
+INSERT INTO specializations(vets_id, species_id)
+VALUES (3, 1);
+
+INSERT INTO specializations(vets_id, species_id)
+VALUES (3, 2);
+
+INSERT INTO specializations(vets_id, species_id)
+VALUES (4, 2);
+
+-- insert data into visits:
+INSERT INTO visits(vets_id, animals_id, date_of_visit)
+VALUES (1, 1, '2020-05-24');
+
+INSERT INTO visits(vets_id, animals_id, date_of_visit)
+VALUES (3, 1, '2020-07-22');
+
+INSERT INTO visits(vets_id, animals_id, date_of_visit)
+VALUES (4, 2, '2021-02-02');
+
+INSERT INTO visits(vets_id, animals_id, date_of_visit)
+VALUES (3, 2, '2020-01-05');
+
+INSERT INTO visits(vets_id, animals_id, date_of_visit)
+VALUES (3, 2, '2020-03-08');
+
+INSERT INTO visits(vets_id, animals_id, date_of_visit)
+VALUES (3, 2, '2020-05-14');
+
+INSERT INTO visits(vets_id, animals_id, date_of_visit)
+VALUES (3, 4, '2021-05-04');
+
+INSERT INTO visits(vets_id, animals_id, date_of_visit)
+VALUES (4, 5, '2021-02-24');
+
+INSERT INTO visits(vets_id, animals_id, date_of_visit)
+VALUES (2, 6, '2019-12-21');
+
+INSERT INTO visits(vets_id, animals_id, date_of_visit)
+VALUES (1, 6, '2020-08-10');
+
+INSERT INTO visits(vets_id, animals_id, date_of_visit)
+VALUES (2, 6, '2021-04-07');
+
+INSERT INTO visits(vets_id, animals_id, date_of_visit)
+VALUES (3, 7, '2019-09-29');
+
+INSERT INTO visits(vets_id, animals_id, date_of_visit)
+VALUES (4, 8, '2020-10-03');
+
+INSERT INTO visits(vets_id, animals_id, date_of_visit)
+VALUES (4, 8, '2020-11-04');
+
+INSERT INTO visits(vets_id, animals_id, date_of_visit)
+VALUES (2, 9, '2019-01-24');
+
+INSERT INTO visits(vets_id, animals_id, date_of_visit)
+VALUES (2, 9, '2019-05-15');
+
+INSERT INTO visits(vets_id, animals_id, date_of_visit)
+VALUES (2, 9, '2020-02-27');
+
+INSERT INTO visits(vets_id, animals_id, date_of_visit)
+VALUES (2, 9, '2020-08-03');
+
+INSERT INTO visits(vets_id, animals_id, date_of_visit)
+VALUES (3, 10, '2020-05-24');
+
+INSERT INTO visits(vets_id, animals_id, date_of_visit)
+VALUES (1, 10, '2021-01-11');
--- schema.sql
+++ schema.sql
@@ -37,3 +37,34 @@ ADD COLUMN species_id integer REFERENCES species(id)
 -- Add column owner_id which is a foreign key referencing the owners table
 ALTER TABLE animals 
 ADD COLUMN owner_id integer REFERENCES owners(id)
+
+-- Create a table named vets with the following columns:
+CREATE TABLE vets(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(100),
+  age INTEGER,
+  date_of_graduation date
+)
+
+-- There is a many-to-many relationship between the tables species and vets: 
+-- a vet can specialize in multiple species, and a species can have multiple 
+-- vets specialized in it. Create a "join table" called specializations to handle this relationship.
+CREATE TABLE specializations(
+  vets_id integer NOT NULL,
+  species_id integer NOT NULL,
+  FOREIGN KEY (vets_id) REFERENCES vets (id) ON DELETE RESTRICT ON UPDATE CASCADE,
+  FOREIGN KEY (species_id) REFERENCES species (id) ON DELETE RESTRICT ON UPDATE CASCADE,
+  PRIMARY KEY (vets_id, species_id)
+)
+
+-- There is a many-to-many relationship between the tables animals and vets: 
+-- an animal can visit multiple vets and one vet can be visited by multiple animals. 
+-- Create a "join table" called visits to handle this relationship, it should also keep track of the date of the visit.
+CREATE TABLE visits(
+  vets_id integer NOT NULL,
+  animals_id integer NOT NULL,
+  date_of_visit date,
+  FOREIGN KEY (vets_id) REFERENCES vets (id) ON DELETE RESTRICT ON UPDATE CASCADE,
+  FOREIGN KEY (animals_id) REFERENCES animals (id) ON DELETE RESTRICT ON UPDATE CASCADE,
+  PRIMARY KEY (vets_id, animals_id)
+)
